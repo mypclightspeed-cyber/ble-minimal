41ee90b9d6f15c21a5788d25583cd447b715cfdf
mypclightspeed-cyber <mypc.lightspeed@gmail.com>
2025-11-08 23:30:19 +0330

    Add files via upload

--- app/src/main/java/com/example/blescan/MeterActivity.kt
+++ app/src/main/java/com/example/blescan/MeterActivity.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.content.Context
 import android.view.*
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
