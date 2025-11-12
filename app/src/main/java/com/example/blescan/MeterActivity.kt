66006a921ebd744b8f88263d3a4e6fe22fb4c3f3
mypclightspeed-cyber <mypc.lightspeed@gmail.com>
2025-11-12 20:17:29 +0330

    Add files via upload

--- app/src/main/java/com/example/blescan/MeterActivity.kt
+++ app/src/main/java/com/example/blescan/MeterActivity.kt
@@ -468,7 +468,7 @@ class MeterActivity : AppCompatActivity() {
         private var label = "SOC"
 
         // radius shrink factor (B1)
-        private val radiusScale = 0.9f
+        private val radiusScale = 1f
 
         private val track = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             color = Color.parseColor("#E5E7EB") // gray-200
@@ -662,7 +662,7 @@ class MeterActivity : AppCompatActivity() {
         private val maxTemp = 95f
         private val middleTemp = 50f
 
-        private val radiusScale = 0.75f
+        private val radiusScale = 0.9f
 
         private val track = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             color = Color.parseColor("#E5E7EB") // gray-200
