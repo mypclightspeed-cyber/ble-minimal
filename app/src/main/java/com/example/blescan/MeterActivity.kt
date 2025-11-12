6538245bccf3cf035e1ebaf13439bccfbe116f2d
mypclightspeed-cyber <mypc.lightspeed@gmail.com>
2025-11-12 23:00:57 +0330

    Add files via upload

--- app/src/main/java/com/example/blescan/MeterActivity.kt
+++ app/src/main/java/com/example/blescan/MeterActivity.kt
@@ -671,7 +671,7 @@ class MeterActivity : AppCompatActivity() {
         private val maxTemp = 95f
         private val middleTemp = 50f
 
-        private val radiusScale = 1f
+        private val radiusScale = 1.5f
 
         private val track = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             color = Color.parseColor("#E5E7EB") // gray-200
