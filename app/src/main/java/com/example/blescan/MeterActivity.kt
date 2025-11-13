7d9d068b1097ce37ccb2d0f469752c0d8c903cf2
mypclightspeed-cyber <mypc.lightspeed@gmail.com>
2025-11-13 06:07:46 +0330

    Add files via upload

--- app/src/main/java/com/example/blescan/MeterActivity.kt
+++ app/src/main/java/com/example/blescan/MeterActivity.kt
@@ -533,7 +533,7 @@ class MeterActivity : AppCompatActivity() {
 
         override fun onDraw(c: Canvas) {
             super.onDraw(c)
-            val pad = 36f
+            val pad = 16f
             val w = width.toFloat()
             val h = height.toFloat()
             val baseSize = min(w - pad * 2, h * 2.0f - pad * 2)
@@ -726,7 +726,7 @@ class MeterActivity : AppCompatActivity() {
 
         override fun onDraw(c: Canvas) {
             super.onDraw(c)
-            val pad = 36f
+            val pad = 16f
             val w = width.toFloat()
             val h = height.toFloat()
             val baseSize = min(w - pad * 2, h * 2.0f - pad * 2)
