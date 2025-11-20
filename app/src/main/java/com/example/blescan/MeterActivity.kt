2c071b849be7e0231827c003cd6c5ac3dd5df070
mypclightspeed-cyber <mypc.lightspeed@gmail.com>
2025-11-20 06:57:12 +0330

    Add files via upload

--- app/src/main/java/com/example/blescan/MeterActivity.kt
+++ app/src/main/java/com/example/blescan/MeterActivity.kt
@@ -48,10 +48,10 @@ class MeterActivity : AppCompatActivity() {
     private val REG_PACK_UNDERVOLTAGE_RELEASE = 0x23     // Corrected from 0x22
     private val REG_FET_CONTROL = 0x24                   // FET control register
 
-    // Factory mode commands
+    // Factory mode commandsDD 5A 01 02 28 28 D0 77
     private val CMD_ENTER_FACTORY = hex("DD 5A 00 02 56 78 FF 30 77")
-//    private val CMD_EXIT_FACTORY = hex("DD 5A 01 02 28 28 D0 77")
-    private val CMD_EXIT_FACTORY_MODE = hex("DD 5A 00 02 55 AA 8B 77")
+    private val CMD_EXIT_FACTORY = hex("DD 5A 00 02 55 AA 8B 77")
+    
     // FET Control commands
     private val CMD_FET_BOTH_ON = hex("DD 5A E1 02 00 00 FF 1D 77")
     
