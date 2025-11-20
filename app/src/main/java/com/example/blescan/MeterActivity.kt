2572c4c3333ca527528d2b172903d58914e508de
mypclightspeed-cyber <mypc.lightspeed@gmail.com>
2025-11-20 06:49:22 +0330

    Add files via upload

--- app/src/main/java/com/example/blescan/MeterActivity.kt
+++ app/src/main/java/com/example/blescan/MeterActivity.kt
@@ -51,7 +51,7 @@ class MeterActivity : AppCompatActivity() {
     // Factory mode commands
     private val CMD_ENTER_FACTORY = hex("DD 5A 00 02 56 78 FF 30 77")
 //    private val CMD_EXIT_FACTORY = hex("DD 5A 01 02 28 28 D0 77")
-    private val CMD_EXIT_FACTORY_MODE = hex("DD 5A 00 02 55 AA 8B 77")   // Exit factory mode
+    private val CMD_EXIT_FACTORY_MODE = hex("DD 5A 00 02 55 AA 8B 77")
     // FET Control commands
     private val CMD_FET_BOTH_ON = hex("DD 5A E1 02 00 00 FF 1D 77")
     
